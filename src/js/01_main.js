bf0105fe83f52d612ae849be8256dcb819a17db8
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-05-28 02:01:10 +0300

    feat: add modal form (html + css + js)

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,16 +1,37 @@
+const modal = document.querySelector(".modal");
+const openModalBtn = document.querySelector("#openModal");
+const showContactsBtn = document.querySelector("#goToContacts");
+
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
   direction: "horizontal",
   loop: false,
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+showContactsBtn.addEventListener("click", (e) => {
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+openModalBtn.addEventListener("click", (e) => {
+  modal.classList.add("modal_open");
+});
+
+modal.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.closest(".modal__close") || target === modal) {
+    modal.classList.remove("modal_open");
+  }
+});
+
+modal.addEventListener("submit", (e) => {
+  e.preventDefault();
+  modal.classList.remove("modal_open");
 });
